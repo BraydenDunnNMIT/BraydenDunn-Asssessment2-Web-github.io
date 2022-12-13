2d631760e7ec6d900ed51c1ab4b85ebf8ac39397
Brayden Dunn <Brayden-Dunn@live.nmit.ac.nz>
2022-12-14 12:52:36 +1300

    First commit cloned over old portfolio code from web502 A2 2022

---
+++ src/Reaperpage/second.js
@@ -0,0 +1,4 @@
+// Javascript function for the return to homepage button.
+function redirectfunc() {
+    window.location.href = "/BraydenDunn-Asssessment2-Web-github.io/src/landingpage/index.html";
+}
